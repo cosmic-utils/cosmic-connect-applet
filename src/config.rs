2495303db7713bb04cbbe4b5f54a5510b8762917
Carson <carsonburke22@gmail.com>
2025-05-25 14:48:39 -0700

    more configs

--- src/config.rs
+++ src/config.rs
@@ -6,17 +6,24 @@ use serde::{self, Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, CosmicConfigEntry, PartialEq, Eq)]
 #[version = 1]
 pub struct FeaturesConfig {
+    /// Whether to show the info splash view on window open
+    pub show_infosplash: bool,
     pub sms_notifications: bool,
     pub clipboard_sync: ClipboardSyncConfig,
     pub share_notifications: ShareNotificationsConfig,
+    pub battery: BatteryConfig,
+    pub telephone: TelephoneConfig,
 }
 
 impl Default for FeaturesConfig {
     fn default() -> Self {
         Self {
+            show_infosplash: true,
             sms_notifications: true,
             clipboard_sync: ClipboardSyncConfig::default(),
             share_notifications: ShareNotificationsConfig::default(),
+            battery: BatteryConfig::default(),
+            telephone: TelephoneConfig::default(),
         }
     }
 }
@@ -46,6 +53,36 @@ impl Default for ShareNotificationsConfig {
         Self {
             // Populate with all identifiable apps by default, probably
             enabled_apps: HashSet::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct BatteryConfig {
+    pub battery_low_notification: bool,
+    pub charged_to_notification: bool,
+    pub battery_full_notification: bool,
+}
+
+impl Default for BatteryConfig {
+    fn default() -> Self {
+        Self {
+            battery_low_notification: true,
+            charged_to_notification: true,
+            battery_full_notification: true,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct TelephoneConfig {
+    pub pause_on_call: bool,
+}
+
+impl Default for TelephoneConfig {
+    fn default() -> Self {
+        Self {
+            pause_on_call: true,
         }
     }
 }
